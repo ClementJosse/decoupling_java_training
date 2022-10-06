4cfca6933da59d2b9d9fe2aeacfdffd2e865c690
JOSSE Clément <josse@et.esiea.fr>
2022-10-06 14:45:40 +0200

    Add ComputerPlayer

---
+++ src/main/java/fr/lernejo/guessgame/ComputerPlayer.java
@@ -0,0 +1,14 @@
+package fr.lernejo.guessgame;
+
+public class ComputerPlayer implements Player{
+
+    @Override
+    public long askNextGuess() {
+        return 0;
+    }
+
+    @Override
+    public void respond(boolean lowerOrGreater) {
+
+    }
+}
