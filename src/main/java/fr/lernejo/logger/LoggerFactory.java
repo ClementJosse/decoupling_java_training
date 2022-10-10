bb91996ff184883297fa7e2eb221d9d1b6c8f6c6
JOSSE Clément <josse@et.esiea.fr>
2022-10-10 11:44:39 +0200

    Fin du projet

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -5,7 +5,7 @@ import java.io.File;
 
 public class LoggerFactory {
     public static Logger getLogger(String name){
-        return new CompositeLogger(new ContextualLogger(name, new ConsoleLogger()), new ContextualLogger(name, new FileLogger("log.txt")));
-
+        //return new CompositeLogger(new ContextualLogger(name,new ConsoleLogger()),new FilteredLogger(new ContextualLogger(name,new FileLogger("log.txt")),message -> !message.contains("simulation")));
+        return new CompositeLogger(new ContextualLogger(name, new ConsoleLogger()), new ContextualLogger(name, new FilteredLogger(new FileLogger("log.txt"), message -> message.contains("simulation"))));
     }
 }
